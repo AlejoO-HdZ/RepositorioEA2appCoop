f665fa00d34a10d86611e9b44c0441a3737807b1
AlejoO <211151669+AlejoO-HdZ@users.noreply.github.com>
2025-09-28 00:21:28 -0500

    1c) feat: Se agrega interfaz Transacción - Método ejecutar() - Método getMonto() docs: se documenta clase Transaccion. Comentarios en atributos y métodos.

--- src/coopRKC/transacciones/Transaccion.java
+++ src/coopRKC/transacciones/Transaccion.java
@@ -1,4 +1,12 @@
 package coopRKC.transacciones;
 
+/** PUNTO 1) MODELO DE DOMINIO
+ *  d) INTERFAZ TRANSACCION, Interfaz que define el comportamiento de una transacción financiera.
+ * Las clases que la implementan deben definir cómo se ejecuta la transacción y cuál es su monto. */
 public interface Transaccion {
+
+    void ejecutar(); // d) METODO EJECUTAR(), Ejecuta la transacción (depósito o retiro) sobre una cuenta.
+
+    double getMonto(); // d) METODO GETMONTO(), Devuelve el monto involucrado en la transacción.
 }
+
