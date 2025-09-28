b701e5596a3532f2c4e32afaa0e088419290d56c
AlejoO <211151669+AlejoO-HdZ@users.noreply.github.com>
2025-09-28 00:23:27 -0500

    1d) feat: Se crean las clases que implementan a Transacción: Deposito y Retiro. docs: se documentan clases Deposito y Retiro. Comentarios en atributos y métodos principales. Ejemplo de Herencia y Polimorfismo.

--- src/coopRKC/transacciones/Deposito.java
+++ src/coopRKC/transacciones/Deposito.java
@@ -1,4 +1,36 @@
 package coopRKC.transacciones;
 
-public class Deposito {
+import coopRKC.excepciones.MontoInvalidoException;
+import coopRKC.modelo.Cuenta;
+
+/** /** PUNTO 1) MODELO DE DOMINIO
+ *  e) CLASE DEPOSITO, Clase que representa un depósito cuenta bancaria.*/
+public class Deposito implements Transaccion { // Con palabra clave "implements", Clase Deposito Implementa la interfaz Transaccion
+    private final Cuenta cuenta; /* Cuenta del depósito */
+    private final double monto; /* Monto a depositar */
+    /**
+     * Constructor que inicializa el depósito con cuenta y monto.
+     * param "cuenta" Cuenta destino del depósito
+     * param "monto" Monto a depositar. */
+    public Deposito(Cuenta cuenta, double monto) {
+        this.cuenta = cuenta;
+        this.monto = monto;
+    }
+
+    /** Herencia y Polimorfismo, "Deposito" implementa el metodo ejecutar(), definido en la interfaz "Transaccion".
+     * Ejecuta el depósito en la cuenta. */
+
+    @Override /* Se sobreescribe metodo ejecutar() */
+    public void ejecutar() {
+        try {
+            cuenta.depositar(monto); /* Llama al metodo depositar de la cuenta. */
+        } catch (MontoInvalidoException e) {
+            System.out.println(" Error en depósito: " + e.getMessage());
+        }
+    }
+
+    @Override /* Se sobreescribe metodo getmonto()*/
+    public double getMonto() {
+        return monto;
+    }
 }
--- src/coopRKC/transacciones/Retiro.java
+++ src/coopRKC/transacciones/Retiro.java
@@ -1,4 +1,41 @@
 package coopRKC.transacciones;
 
-public class Retiro {
+import coopRKC.excepciones.MontoInvalidoException;
+import coopRKC.excepciones.SaldoInsuficienteException;
+import coopRKC.modelo.Cuenta;
+
+
+/** /** PUNTO 1) MODELO DE DOMINIO
+ *  e) CLASE RETIRO, Clase que representa un retiro cuenta bancaria.*/
+public class Retiro implements Transaccion { // Con palabra clave "implements", Clase Retiro Implementa la interfaz Transaccion
+    private final Cuenta cuenta;
+    private final double monto;
+
+    /**
+     * Constructor que inicializa el retiro con cuenta y monto.
+     * param cuenta Cuenta origen del retiro
+     * param monto Monto a retirar
+     */
+    public Retiro(Cuenta cuenta, double monto) {
+        this.cuenta = cuenta;
+        this.monto = monto;
+    }
+
+    /** 4) GESTION DE ERRORES, Controlar retiros cuando el saldo no sea suficiente.
+     * Ejecuta el retiro si hay saldo suficiente.
+     * Muestra un mensaje si el retiro no es posible, a partir de las coopRKC.excepciones
+     */
+    @Override
+    public void ejecutar() {
+        try {
+            cuenta.retirar(monto);
+        } catch (SaldoInsuficienteException | MontoInvalidoException e) {
+            System.out.println(" Error en retiro: " + e.getMessage());
+        }
+    }
+
+    @Override /* Se sobreescribe metodo ejecutar()*/
+    public double getMonto() {
+        return monto;
+    }
 }
